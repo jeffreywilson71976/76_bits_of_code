d4bb5e90919506d9a72abf0d694101e868e62685
76bits <jeffrey.wilson71976@gmail.com>
2026-06-23 10:44:34 +0200

    Update decode.py
    Refactor into a single function and added comments on what is being done at each step

--- decode.py
+++ decode.py
@@ -12,33 +12,28 @@ from bs4 import BeautifulSoup
 def fetch_google_doc(google_doc):
     table = pd.read_html(google_doc, flavor = 'bs4', header=0)
     df = table[0] # as there is only one table in the doc
-    return df
+    
+    # define variables for the plot function (x_cord, y_cord, char) as 
+    # lists of values from the DataFrame columns
+    x_cord = [int(i) for i in list(df['x-coordinate'])]
+    y_cord = [int(i) for i in list(df['y-coordinate'])]
+    char = list(df['Character'])
 
-# Define function to plot the characters on a grid using numpy
-def plot_char(x_cord:list, y_cord:list, char:list):
+    # get max values from x_cord and y_cord, to decide the max length for the grid
     max_x = max(x_cord)
     max_y = max(y_cord)
+
     grid = np.full((max_y+1, max_x+1), ' ') # need to add 1 to the max to ensure that the index from the list is converted to the correct dimensions
     for i in range(len(x_cord)):
+        # Reversing x and y coordinates to adjust the grid
         grid[y_cord[i]][x_cord[i]] = char[i]
+    # Create an empty grid to insert the values from the first grid and transverse the values backwards    
     grid_2 = []
     for i in range(len(grid)-1, -1 , -1):
-        grid_2.append(grid[i])
+        grid_2.append(grid[i])    
     for row in grid_2:
         print(''.join(row))
 
-
+# Set the google doc url to variable and call the Google Doc via call to the fetch function
 google_doc = 'https://docs.google.com/document/d/e/2PACX-1vSvM5gDlNvt7npYHhp_XfsJvuntUhq184By5xO_pA4b_gCWeXb6dM6ZxwN8rE6S4ghUsCj2VKR21oEP/pub'
-
-# Call the Google Doc via call to the fetch function
-google_doc_df = fetch_google_doc(google_doc)
-
-# define variables for the plot function (x_cord, y_cord, char) as 
-# lists of values from the DataFrame columns
-x_cord = [int(i) for i in list(google_doc_df['x-coordinate'])]
-y_cord = [int(i) for i in list(google_doc_df['y-coordinate'])]
-char = list(google_doc_df['Character'])
-
-# call the plot function to print out the hidden message passing the lists 
-plot_char(x_cord, y_cord, char)
-
+fetch_google_doc(google_doc)
